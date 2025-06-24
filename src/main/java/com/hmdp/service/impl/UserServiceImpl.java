676e02eab148a44b72fc06bff5a9c2af98523c22
致爱蓝海 <1595711067@qq.com>
2025-06-24 21:41:13 +0800

    添加用户服务登录、注册与验证码发送功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,17 +1,95 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.VariousConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
  * 服务实现类
  * </p>
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送手机验证码
+     */
+    public Result sendCode(String phone, HttpSession session) {
+        //1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //2.不符合，返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        //符合,3.生成验证码
+        //引用胡图工具包，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //4.保存验证码到session
+        session.setAttribute(VariousConstants.VERIFICATION_CODE, code);
+        //5.发送验证码（暂时不做真正的发送验证码，需要调用阿里云短信平台等）
+        log.debug("发送验证码成功，验证码:{}", code);
+        //返回OK
+        return Result.ok();
+    }
+
+    /**
+     * 登录功能
+     *
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //2.不符合，返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        //2.校验验证码
+        Object cacheCode = session.getAttribute(VariousConstants.VERIFICATION_CODE);
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            //3.不一致，报错
+            return Result.fail("验证码错误");
+        }
+
+        //4.一致，判断手机号是否存在 select * from tb_user where phone = ?
+        User user = query().eq("phone", phone).one();
+        //5.判断用户是否存在
+        if (user == null) {
+            //6.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        //7.保存用户信息到session中
+        session.setAttribute(VariousConstants.USER, user);
+        return Result.ok();
+    }
+
+    /**
+     * 用手机号注册新用户
+     *
+     * @param phone
+     * @return
+     */
+    private User createUserWithPhone(String phone) {
+        //1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        //随机生成用户名
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
---
+++ src/main/java/com/hmdp/utils/VariousConstants.java
@@ -0,0 +1,10 @@
+package com.hmdp.utils;
+
+/**
+ * 各种常量
+ */
+public class VariousConstants {
+
+    public static final String VERIFICATION_CODE = "code";
+    public static final String USER = "user";
+}
