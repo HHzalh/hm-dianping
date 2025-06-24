2700ac442a418a4f3051cccf63a05f00c4dc2f6b
致爱蓝海 <1595711067@qq.com>
2025-06-24 20:57:51 +0800

    黑马点评-项目初始化

---
+++ src/main/java/com/hmdp/service/IUserInfoService.java
@@ -0,0 +1,13 @@
+package com.hmdp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.entity.UserInfo;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ */
+public interface IUserInfoService extends IService<UserInfo> {
+
+}
