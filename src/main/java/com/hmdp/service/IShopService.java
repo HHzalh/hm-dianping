e4658327b355892f037b12ae2f3c4250d7c90c0a
致爱蓝海 <1595711067@qq.com>
2025-06-25 21:04:37 +0800

    添加商铺查询缓存及优化登出逻辑

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 
 /**
@@ -10,4 +11,11 @@ import com.hmdp.entity.Shop;
  */
 public interface IShopService extends IService<Shop> {
 
+    /**
+     * 根据id查询商铺信息
+     *
+     * @param id 商铺id
+     * @return 商铺详情数据
+     */
+    Result queryById(Long id);
 }
