aec7124f37b5f52313a0e299ef40d3a278c531f5
致爱蓝海 <1595711067@qq.com>
2025-06-25 15:10:18 +0800

    优化登录逻辑，使用Redis存储验证码和用户信息

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -9,13 +11,18 @@ import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RegexUtils;
 import com.hmdp.utils.SystemConstants;
-import com.hmdp.utils.VariousConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -26,6 +33,9 @@ import javax.servlet.http.HttpSession;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     /**
      * 发送手机验证码
      */
@@ -35,11 +45,15 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             //2.不符合，返回错误信息
             return Result.fail("手机号格式错误");
         }
+
         //符合,3.生成验证码
         //引用胡图工具包，生成验证码
         String code = RandomUtil.randomNumbers(6);
-        //4.保存验证码到session
-        session.setAttribute(VariousConstants.VERIFICATION_CODE, code);
+        //session.setAttribute(VariousConstants.VERIFICATION_CODE, code);  保存验证码到session中
+
+        //4.保存验证码到redis,并设置有效期 2分钟
+        stringRedisTemplate.opsForValue().set(RedisConstants.LOGIN_CODE_KEY + phone, code, RedisConstants.LOGIN_CODE_TTL, TimeUnit.MINUTES);
+
         //5.发送验证码（暂时不做真正的发送验证码，需要调用阿里云短信平台等）
         log.debug("发送验证码成功，验证码:{}", code);
         //返回OK
@@ -58,11 +72,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             //2.不符合，返回错误信息
             return Result.fail("手机号格式错误");
         }
-        //2.校验验证码
-        Object cacheCode = session.getAttribute(VariousConstants.VERIFICATION_CODE);
+        //3.从redis中获取验证码，并校验
+        //Object cacheCode = session.getAttribute(VariousConstants.VERIFICATION_CODE); 从session中获取验证码
+        String cacheCode = stringRedisTemplate.opsForValue().get(RedisConstants.LOGIN_CODE_KEY + phone);
         String code = loginForm.getCode();
-        if (cacheCode == null || !cacheCode.toString().equals(code)) {
-            //3.不一致，报错
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            //不一致，报错
             return Result.fail("验证码错误");
         }
 
@@ -74,10 +89,40 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             user = createUserWithPhone(phone);
         }
 
-        //7.保存用户信息到session中
+        //7.保存用户信息到redis中
+
+        //7.1随机生成token，作为登录令牌
+
+        //引入胡图工具类的UUID
+        String token = UUID.randomUUID().toString(true);
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        //7.2 利用胡图工具类，将UserDto对象转换为HashMap存储
+
+
+        //stringRedisTemplate 中的 putAll 方法在存储数据到 Redis 时，会将所有的值都序列化为字符串。
+        // 因此，如的 Map 中包含 Long 类型的值，需要先将它们转换为字符串，以避免类型转换异常。
+
+        // 创建一个空的HashMap，用于存储转换后的用户信息
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
+                // 使用CopyOptions.create()创建一个CopyOptions对象，用于配置转换选项
+                CopyOptions.create()
+                        // 设置忽略空值，即在转换过程中不将空值放入Map中
+                        .setIgnoreNullValue(true)
+                        // 设置字段值编辑器，用于在转换过程中对字段值进行自定义处理
+                        // 这里使用了一个Lambda表达式，将字段值转换为字符串
+                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+
+
         //引入胡图 BeanUtil.copyProperties 用户信息脱敏
-        session.setAttribute(VariousConstants.USER, BeanUtil.copyProperties(user, UserDTO.class));
-        return Result.ok();
+        //session.setAttribute(VariousConstants.USER, BeanUtil.copyProperties(user, UserDTO.class));
+
+        //7.3存储
+        stringRedisTemplate.opsForHash().putAll(RedisConstants.LOGIN_USER_KEY + token, userMap);
+        //7.4设置token有效期 30分钟
+        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY + token, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+
+        //8.返回token
+        return Result.ok(token);
     }
 
     /**
