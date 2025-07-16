b40da238d38f18f597dc02e820bde8e72f10422e
致爱蓝海 <1595711067@qq.com>
2025-07-16 23:37:20 +0800

    基于Redis实现全局唯一ID以及编写单元测试类

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,19 +1,62 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    private final ExecutorService es = Executors.newFixedThreadPool(500);
+
     @Resource
     private ShopServiceImpl shopService;
 
+    @Resource
+    private CacheClient cacheClient;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
     @Test
-    public void test() throws InterruptedException {
-        shopService.saveShop2Redis(1L, 10L);
+    void testIdWorker() throws InterruptedException {
+        // 用于等待所有任务完成，初始计数为300
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            // 每个任务完成后，将CountDownLatch的计数减1
+            latch.countDown();
+        };
+        // 记录开始时间
+        long begin = System.currentTimeMillis();
+        // 提交300个任务到线程池es
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        // 等待所有任务完成
+        latch.await();
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
+
+    @Test
+    void testSaveShop() throws InterruptedException {
+        Shop shop = shopService.getById(1L);
+        cacheClient.setWithLogicalExpire(RedisConstants.CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
 }
